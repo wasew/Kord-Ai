a26ea50bd6484febed8a6f89b86fdcb9b61c2cb8
wasew <winnersnakuru8@gmail.com>
2024-11-23 23:15:29 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,18 +1,18 @@
 global.settings = {
-    OWNER_NUMBERS: "2347013159244", // replace with your number
+    OWNER_NUMBERS: "254727293652", // replace with your number
     OWNER_NAME: 'Miracle', // desired name
     BOT_NAME: "KORD", // desired name
     VERSION: 'V1.0.0',
     SESSION_ID: '', //put your session id between ''
-    MONGO_DB_URL: 'mongodb+srv://miracle32669:Iyanu1234@cluster0.m7glm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    MONGO_DB_URL: 'mongodb+srv://nepaj57019:<db_password>@clus345.e8qv0.mongodb.net/?retryWrites=true&w=majority&appName=Clus345',
     WORK_MODE: "Public",
-    TIME_ZONE: "Africa/Lagos",
+    TIME_ZONE: "Africa/Nairobi",
     ANTI_VIEWONCE: true,
     AUTO_READ_STATUS: true,
     ALWAYS_ONLINE: true,
     READ_ALL_MESSAGES: false,
     PREFIX: [".", "/", "!"],
-    WELCOME_MESSAGE: "Hello! I'm KORD-AI, your friendly WhatsApp bot.",
+    WELCOME_MESSAGE: "Hello! I'm PRINCE-Ai, your friendly WhatsApp bot.",
     DEFAULT_TRANSLATION_LANG: "en",
     COMMAND_COOLDOWN_TIME_IN_MS: 2000,
     MAX_COMMANDS_PER_MINUTE: 10,
